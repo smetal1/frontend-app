32d72987165cc76b893c28e927b9934de0a8485f
a840725 <67306714+a840725@users.noreply.github.com>
2020-08-11 09:43:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import Axios from 'axios';
+import Bootstraptable from 'react-bootstrap-table-next';
+import pagination from 'react-bootstrap-table2-paginator';
+import * as ReactBootstrap from 'react-bootstrap'
 
 function App() {
+
+  const [city,setCity]=useState([]);
+  const [loading,setLoading]=useState(false);
+  const getCity=async()=>{
+    try {
+      var config = {
+        headers: {'Access-Control-Allow-Origin': '*'}
+    };
+      const data = await Axios.get("http://localhost:3001",config);
+      setCity(data.data)
+      setLoading(true)
+    }catch(e){
+      console.log("ERROR-",e)
+    }
+  }
+  const columns =[
+    {dataField: "ID", text: "Index"},
+    {dataField: "Name", text: "Name"},
+    {dataField: "CountryCode", text: "Country Code"},
+    {dataField: "District", text: "District"},
+    {dataField: "Population", text: "Population"}
+  ]
+
+  useEffect(()=>{
+    getCity()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+         <Bootstraptable
+         keyField="ID"
+         data= {city}
+         columns={columns}
+         pagination={pagination()}
+         />
+      ): (<ReactBootstrap.Spinner animaton="border"></ReactBootstrap.Spinner>)}
+    
     </div>
   );
 }
